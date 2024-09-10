3b0660e7bc9613ecaabde3ba384113c851b16b1d
Jesse Bullard <jessebullard1996@gmail.com>
2024-09-10 14:42:27 -0600

    Finished creating all tests for pokemon controller and cleaned up unneeded variables in tests

--- ReviewApp.Test/Controller/PokemonControllerTests.cs
+++ ReviewApp.Test/Controller/PokemonControllerTests.cs
@@ -89,8 +89,6 @@ namespace ReviewApp.Test.Controller
         {
             // arrange
             int pokeId = 1;
-            var pokemon = A.Fake<Pokemon>();
-            var pokemonDto = A.Fake<PokemonDto>();
 
             A.CallTo(() => _pokemonRepository.PokemonExists(pokeId)).Returns(false);
 
@@ -206,6 +204,24 @@ namespace ReviewApp.Test.Controller
         }
 
         [Fact]
+        public void PokemonController_CreatePokemon_Return400BadRequest_ForNullInput()
+        {
+            // arrange
+            int ownerId = 1;
+            int categoryId = 2;
+            PokemonDto pokemonCreate = null;
+
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            //act
+            var result = controller.CreatePokemon(ownerId, categoryId, pokemonCreate);
+
+            // assert
+            result.Should().BeOfType<BadRequestObjectResult>();
+            result.As<BadRequestObjectResult>().StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+        }
+
+        [Fact]
         public void PokemonController_CreatePokemon_Return422UnprocessableEntity_ForExistingPokemon()
         {
             // arrange
@@ -229,7 +245,7 @@ namespace ReviewApp.Test.Controller
         }
 
         [Fact]
-        public void PokemonController_CreatePokemon_Return500InternalServerError_ForExistingPokemon()
+        public void PokemonController_CreatePokemon_Return500InternalServerError()
         {
             // arrange
             int ownerId = 1;
@@ -237,7 +253,7 @@ namespace ReviewApp.Test.Controller
             var pokemonCreate = A.Fake<PokemonDto>();
             var pokemonMap = A.Fake<Pokemon>();
 
-            A.CallTo(() => _pokemonRepository.GetPokemonTrimToUpper(pokemonCreate)).Returns(pokemonMap);
+            A.CallTo(() => _pokemonRepository.GetPokemonTrimToUpper(pokemonCreate)).Returns(null);
             A.CallTo(() => _mapper.Map<Pokemon>(pokemonCreate)).Returns(pokemonMap);
             A.CallTo(() => _pokemonRepository.CreatePokemon(ownerId, categoryId, pokemonMap)).Returns(false);
 
@@ -248,7 +264,134 @@ namespace ReviewApp.Test.Controller
 
             // assert
             result.Should().BeOfType<ObjectResult>()
-                .Which.StatusCode.Should().Be(StatusCodes.Status422UnprocessableEntity);
+                .Which.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+        }
+
+        [Fact]
+        public void PokemonController_CreatePokemon_Return500InternalServerErrorOnException()
+        {
+            // arrange
+            int ownerId = 1;
+            int categoryId = 2;
+            var pokemonCreate = A.Fake<PokemonDto>();
+
+            A.CallTo(() => _pokemonRepository.GetPokemonTrimToUpper(pokemonCreate)).Throws(new Exception());
+
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            //act
+            var result = controller.CreatePokemon(ownerId, categoryId, pokemonCreate);
+
+            // assert
+            result.Should().BeOfType<ObjectResult>()
+                .Which.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+        }
+
+        [Fact]
+        public void PokemonController_DeletePokemon_Return204NoContent_OnSuccess()
+        {
+            // arrange
+            int pokeId = 1;
+            var reviews = new List<Review>();
+            var pokemonToDelete = new Pokemon { Id = pokeId, Name = "Pikachu" };
+
+            A.CallTo(() => _pokemonRepository.PokemonExists(pokeId)).Returns(true);
+            A.CallTo(() => _reviewRepository.GetReviewsOfAPokemon(pokeId)).Returns(reviews);
+            A.CallTo(() => _pokemonRepository.GetPokemon(pokeId)).Returns(pokemonToDelete);
+            A.CallTo(() => _reviewRepository.DeleteReviews(reviews)).Returns(true);
+            A.CallTo(() => _pokemonRepository.DeletePokemon(pokemonToDelete)).Returns(true);
+
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            //act
+            var result = controller.DeletePokemon(pokeId);
+
+            // assert
+            result.Should().BeOfType<NoContentResult>()
+            .Which.StatusCode.Should().Be(StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        public void PokemonController_DeletePokemon_Return404NotFound()
+        {
+            // arrange
+            int pokeId = 1;
+
+            A.CallTo(() => _pokemonRepository.PokemonExists(pokeId)).Returns(false);
+
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            //act
+            var result = controller.DeletePokemon(pokeId);
+
+            // assert
+            result.Should().BeOfType<NotFoundResult>()
+            .Which.StatusCode.Should().Be(StatusCodes.Status404NotFound);
+        }
+
+        [Fact]
+        public void PokemonController_DeletePokemon_Return500NoInternalServerError_OnErrorDeletingReviews()
+        {
+            // arrange
+            int pokeId = 1;
+            var reviews = new List<Review>();
+            var pokemonToDelete = new Pokemon { Id = pokeId, Name = "Pikachu" };
+
+            A.CallTo(() => _pokemonRepository.PokemonExists(pokeId)).Returns(true);
+            A.CallTo(() => _reviewRepository.GetReviewsOfAPokemon(pokeId)).Returns(reviews);
+            A.CallTo(() => _pokemonRepository.GetPokemon(pokeId)).Returns(pokemonToDelete);
+            A.CallTo(() => _reviewRepository.DeleteReviews(reviews)).Returns(false);
+
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            //act
+            var result = controller.DeletePokemon(pokeId);
+
+            // assert
+            result.Should().BeOfType<ObjectResult>()
+            .Which.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+        }
+
+        [Fact]
+        public void PokemonController_DeletePokemon_Return500NoInternalServerError_OnErrorDeletingPokemon()
+        {
+            // arrange
+            int pokeId = 1;
+            var reviews = new List<Review>();
+            var pokemonToDelete = new Pokemon { Id = pokeId, Name = "Pikachu" };
+
+            A.CallTo(() => _pokemonRepository.PokemonExists(pokeId)).Returns(true);
+            A.CallTo(() => _reviewRepository.GetReviewsOfAPokemon(pokeId)).Returns(reviews);
+            A.CallTo(() => _pokemonRepository.GetPokemon(pokeId)).Returns(pokemonToDelete);
+            A.CallTo(() => _reviewRepository.DeleteReviews(reviews)).Returns(true);
+            A.CallTo(() => _pokemonRepository.DeletePokemon(pokemonToDelete)).Returns(false);
+
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            //act
+            var result = controller.DeletePokemon(pokeId);
+
+            // assert
+            result.Should().BeOfType<ObjectResult>()
+            .Which.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
+        }
+
+        [Fact]
+        public void PokemonController_DeletePokemon_Return500NoInternalServerError_OnException()
+        {
+            // arrange
+            int pokeId = 1;
+
+            A.CallTo(() => _pokemonRepository.PokemonExists(pokeId)).Throws(new Exception());
+
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            //act
+            var result = controller.DeletePokemon(pokeId);
+
+            // assert
+            result.Should().BeOfType<ObjectResult>()
+            .Which.StatusCode.Should().Be(StatusCodes.Status500InternalServerError);
         }
     }
 }
